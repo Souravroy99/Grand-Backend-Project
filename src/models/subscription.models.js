54a4adbd4a5a76503af23d80c9266a58277efa68
Souravroy99 <souravroy200418@gmail.com>
2024-12-25 23:44:26 +0530

    Aggregation Pipeline

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,19 +1,19 @@
-import mongoose from "mongoose" ;
+import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({
-    subscriber: {
-        type: mongoose.Schema.Types.ObjectId , // One who is subscribing
-        ref: "User"
+const subscriptionSchema = new mongoose.Schema(
+    {
+        subscriber: {
+            type: mongoose.Schema.Types.ObjectId, // One who is subscribing
+            ref: "User",
+        },
+        channel: {
+            type: mongoose.Schema.Types.ObjectId, // Subscriber's channel
+            ref: "User",
+        },
     },
-    channel: {
-        type: mongoose.Schema.Types.ObjectId , // Subscriber
-        ref: "User"
+    {
+        timestamps: true,
     }
-}, 
-{
-    timestamps: true
-}
 );
 
-
-export const subscription = mongoose.model("subscription", subscriptionSchema); 
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
