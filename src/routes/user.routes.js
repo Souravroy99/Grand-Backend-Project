ef6de56ec27301d292c95c5035d2abc021546f4b
Souravroy99 <souravroy200418@gmail.com>
2024-12-28 01:05:46 +0530

    Add new routes for user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,33 +1,35 @@
-import {Router} from "express" ;
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage} from "../controllers/user.controller.js";
 
-
-const router = Router() ;
+const router = Router();
 
 router.route("/register").post(
-    upload.fields(
-        [
-            { name: "avatar", maxCount: 1},
-            { name: "coverImage", maxCount: 1}
-        ]
-    ),    
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        { name: "coverImage", maxCount: 1 },
+    ]),
 
     registerUser
 );
-router.route("/login").post(loginUser)
-
+router.route("/login").post(loginUser);
 
 // Secured Routes
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken) // verifyJWT is not required because I check the user's existence inside of the function, but we can use it as well.
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken); // verifyJWT is not required because I check the user's existence inside of the function, but we can use it as well.
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router.route("/avatar").patch(verifyJWT, upload.single({ name: "avatar" }), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 
 
-export default router ;
-
-
-
+export default router;
 
 // maxCount --> The maximum number of files allowed for that field.
